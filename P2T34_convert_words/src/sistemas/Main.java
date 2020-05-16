90e7b148aa27e72fab80df6569f9d6189125194d
Brayan Nicolas Quiroz Parra <brayan.quiroz@usantoto.edu.co>
2020-05-15 23:18:03 -0500

    ejercicio
    ejercicio

---
+++ P2T34_convert_words/src/sistemas/Main.java
@@ -0,0 +1,40 @@
+package sistemas;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*/*AUTHOR BRAYAN NICOLAS QUIROZ PARRA
+	  DATE 12/05/2020
+	  DESCRIPTION
+
+	 */
+        f_menu();
+        f_names();
+    }
+
+    public static void f_menu() {
+        //description this method show the menu of this software
+        System.out.println("====================================");
+        System.out.println("|        softConvertWords          |");
+        System.out.println("|     version 1.0 12/05/2020       |");
+        System.out.println("|    created by: Nicolas Quiroz    |");
+        System.out.println("====================================");
+
+    }
+    public static String[][] f_names(){
+        String[][] names = new String[4][5];
+
+        String [][] matriz = {{"maria","pedro","jose","camilo","rodrigo"},
+                {"maria","pedro","jose","camilo","rodrigo"},{"maria","pedro","jose","camilo","rodrigo"}};
+        String nombres[] = {"dsds","sdsd","dsds","dsdsd"};
+
+
+
+
+return names;
+
+    }
+
+
+}
+
